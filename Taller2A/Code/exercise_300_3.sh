6accc6d0ba1684595bbd383691010fb21cb0d83b
este6an13 <diquintero@unal.edu.co>
2023-02-16 04:27:38 +0000

    flags swap, remove indent

--- Taller2A/Code/exercise_300_3.sh
+++ Taller2A/Code/exercise_300_3.sh
@@ -1,1 +1,1 @@
-grep -v -B1 "A" ../Data/mature.fa | grep '>' | cut -d ' ' -f3,4 | sort | uniq > ../Results/exercise_300_3.out
+grep -B1 -v "A" ../Data/mature.fa | grep '>' | cut -d ' ' -f3,4 | sort | uniq > ../Results/exercise_300_3.out
